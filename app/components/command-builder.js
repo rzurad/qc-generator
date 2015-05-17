c8c6f8d34946c2224352fde398f1091a37e07a88
Richard Zurad <rzurad@gmail.com>
2015-05-16 17:29:38 -0700

    Stop

--- app/components/command-builder.js
+++ app/components/command-builder.js
@@ -4,19 +4,24 @@ import { argFactory } from '../models/args';
 import { CMD_TYPES, commandFactory } from '../models/commands';
 
 export default Ember.Component.extend({
+    tagName: 'li',
     classNames: ['command-builder'],
     command: null,
     args: [],
     commandKeys: Object.keys(CMD_TYPES),
     showComment: false,
 
-    onShowCommentChange: function () {
-        if (this.get('showComment')) {
+    onInit: function () {
+        var comment = this.get('command.comment');
+
+        if (!!comment) {
+            this.set('showComment', !!comment);
+
             Ember.run.scheduleOnce('afterRender', this, function () {
-                Ember.$(this.get('element')).find('.comment').focus();
+                Ember.$(this.get('element')).find('.comment').text(comment.replace(/^\/\/\ /g, ''));
             });
         }
-    }.observes('showComment'),
+    }.on('init'),
 
     onCommandChange: function () {
         var command = this.get('command');
@@ -38,7 +43,7 @@ export default Ember.Component.extend({
         var $target = Ember.$(e.target);
 
         if ($target.is('.command')) {
-            this.set('command', commandFactory($target.val()));
+            this.set('command', commandFactory($target.val(), this.get('command.comment')));
         }
     },
 
@@ -55,8 +60,12 @@ export default Ember.Component.extend({
     },
 
     actions: {
-        showComment: function () {
+        addComment: function () {
             this.set('showComment', true);
+
+            Ember.run.scheduleOnce('afterRender', this, function () {
+                Ember.$(this.get('element')).find('.comment').focus();
+            });
         }
     }
 });
